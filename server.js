6c0bcba5db8df116403c03fe236e197fc4fe12e8
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-11-14 01:34:51 +0000

    update server js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = 3000
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
+
+app.post('/quotes', (req, res) => { 
+    console.log('YEl')
+})
 app.listen(PORT, () => { 
     console.log(`Server is running at http://localhost:${PORT}`);
 });
