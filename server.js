d8c4771a226ef577752404d3bb457bfd65cc5870
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-11-14 01:13:10 +0000

    commit

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 
 const PORT = 3000
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 app.listen(PORT, () => { 
     console.log(`Server is running at http://localhost:${PORT}`);
 });
