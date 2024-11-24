c568f07f3017d33941939b5027635eb50d4bfeb5
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-11-24 20:40:31 +0000

    commit

--- server.js
+++ server.js
@@ -68,6 +68,9 @@ app.delete('/quotes', (req,res) => {
     quotesCollection
         .deleteOne({name: req.body.name})
         .then(result => {
+            if (result.deletedCount === 0) { 
+                return res.json('No quote to delete');
+            }
             res.json(`Deleted Darth Vader's quote`)
         })
     .catch(error => {console.error(error)});
